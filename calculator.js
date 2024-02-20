846f6fa15947bfb230e34a7b0a940ffac14a352a
Julien-MC <148296324+Julien-MC@users.noreply.github.com>
2024-02-20 14:21:09 -0800

    calculator.js

--- calculator.js
+++ calculator.js
@@ -1,2 +1,2 @@
 /// alert.js
-javascript:window.onkeydown = function(event) {   if (event.keyCode === 190) {      javascript:eval('function calc(){_o=prompt(_t,_z);if(_o!=\'\'&&_o!=null&&_o.toUpperCase()==_o.toLowerCase())_z=eval(_o);}');_t='JAVASCRIPTER.NET Calculator - Input the expression to be calculated:';_z='';calc();while(_o!=''&&_o!=null&&_o.toUpperCase()==_o.toLowerCase())calc()   }};
+javascript:window.onkeydown = function(event) {   if (event.keyCode === 75) {      javascript:eval('function calc(){_o=prompt(_t,_z);if(_o!=\'\'&&_o!=null&&_o.toUpperCase()==_o.toLowerCase())_z=eval(_o);}');_t='FrostMC Calculator - Input the expression to be calculated:';_z='';calc();while(_o!=''&&_o!=null&&_o.toUpperCase()==_o.toLowerCase())calc()   }};
